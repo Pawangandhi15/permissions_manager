4d0b066253d95eabbc6959ebdc553db88620fa6c
aditya-a-wadikar <aditya.a.wadikar@gmail.com>
2024-06-04 02:07:27 +0530

    Initial Commit

--- project/models.py
+++ project/models.py
@@ -3,3 +3,7 @@ from django.db import models
 class Project(models.Model):
     project_id = models.AutoField(primary_key=True)
     project_name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f"{self.project_name}"
+
--- task/models.py
+++ task/models.py
@@ -5,4 +5,4 @@ class Task(models.Model):
     task_name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
-        return f"{self.task}"
+        return f"{self.task_name}"
